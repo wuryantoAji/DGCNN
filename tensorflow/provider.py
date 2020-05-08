466081e7abab91fa3a592196adbc22870dea6366
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-05-08 22:40:16 +0700

    add data_dir

--- tensorflow/provider.py
+++ tensorflow/provider.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
 # Download dataset for point cloud classification
-#DATA_DIR = '/home/aji/aji-skripsi/data'
+DATA_DIR = '/home/aji/aji-skripsi/data'
 #if not os.path.exists(DATA_DIR):
 #  os.mkdir(DATA_DIR)
 #if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
@@ -149,9 +149,9 @@ def load_h5(h5_filename):
   return (data, label)
 
 def loadDataFile(filename):
-  #print(f'{DATA_DIR}/{filename}')
-  print(f'{filename}')
-  return load_h5(f'{filename}')
+  print(f'{DATA_DIR}/{filename}')
+  #print(f'{filename}')
+  return load_h5(f'{DATA_DIR}/{filename}')
 
 
 def load_h5_data_label_seg(h5_filename):
