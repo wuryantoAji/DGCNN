1c5d2600e7bdd9965db09f0d53191c7ac3352534
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-05-09 12:38:09 +0700

    add num_gpu

--- tensorflow/sem_seg/train.py
+++ tensorflow/sem_seg/train.py
@@ -21,6 +21,7 @@ from model import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--model', default='dgcnn', help='Model name: dgcnn')
+parser.add_argument('--num_gpu', type=int, default=1, help='the number of GPUs to use [default: 2]')
 parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
 parser.add_argument('--num_point', type=int, default=4096, help='Point number [default: 4096]')
 parser.add_argument('--max_epoch', type=int, default=51, help='Epoch to run [default: 50]')
