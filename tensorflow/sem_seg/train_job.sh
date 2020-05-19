0c19f91c65a42e9e03444c25e31e6f63ac2cad54
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-05-19 11:28:58 +0700

    batch inference for dublin

--- tensorflow/sem_seg/train_job.sh
+++ tensorflow/sem_seg/train_job.sh
@@ -1,1 +1,6 @@
-python3 train.py --log_dir log1 --test_area 1
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log1_dublin --test_area 1 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log2_dublin --test_area 2 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log3_dublin --test_area 3 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log4_dublin --test_area 4 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log5_dublin --test_area 5 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 train.py --log_dir log6_dublin --test_area 6 &
