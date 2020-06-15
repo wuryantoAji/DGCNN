0bde8abbac8d4ae5d10e7b0bff92b992c2d8dbb1
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-06-15 11:51:28 +0700

    change label

--- tensorflow/sem_seg/eval_all.py
+++ tensorflow/sem_seg/eval_all.py
@@ -6,7 +6,7 @@ for i in range(1,7):
   file_name = 'log{}/output_filelist_RGBFormat.txt'.format(i)
   pred_data_label_filenames += [line.rstrip() for line in open(file_name)]
 
-gt_label_filenames = [f.rstrip('_pred\.txt') + '_gt.txt' for f in pred_data_label_filenames]
+gt_label_filenames = [f.rstrip('_pred_change_format\.txt') + '_gt_change_format.txt' for f in pred_data_label_filenames]
 
 num_room = len(gt_label_filenames)
 
