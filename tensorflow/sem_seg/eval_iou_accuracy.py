4e3a9b2ec95a0140116a39db622e5d944dda1a9d
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-06-15 15:17:41 +0700

    add change format

--- tensorflow/sem_seg/eval_iou_accuracy.py
+++ tensorflow/sem_seg/eval_iou_accuracy.py
@@ -9,7 +9,7 @@ pred_data_label_filenames = []
 file_name = 'log{}/output_filelist_RGBFormat.txt'.format(FLAGS.area)
 pred_data_label_filenames += [line.rstrip() for line in open(file_name)]
 
-gt_label_filenames = [f.rstrip('_pred\.txt') + '_gt.txt' for f in pred_data_label_filenames]
+gt_label_filenames = [f.rstrip('_pred_change_format\.txt') + '_gt_change_format.txt' for f in pred_data_label_filenames]
 
 num_room = len(gt_label_filenames)
 
