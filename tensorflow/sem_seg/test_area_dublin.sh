0d0cbc14db449191a6da83d5d488e89d399d2bfe
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-06-19 14:34:04 +0700

    test_area_dublin

--- tensorflow/sem_seg/test_area_dublin.sh
+++ tensorflow/sem_seg/test_area_dublin.sh
@@ -1,17 +1,9 @@
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log1/epoch_49.ckpt --dump_dir log1/prediction --output_filelist log1/prediction_all.txt --room_data_filelist meta/area1_data_label_margonda.txt --area 1 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log1/epoch_49.ckpt --dump_dir log1/prediction --output_filelist log1/prediction_build_only.txt --room_data_filelist meta/area1_data_label_margonda.txt --area 1 --visu --jenis dublin  
+CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log1_dublin_notShifted/epoch_49.ckpt --dump_dir log1_dublin_notShifted/prediction --output_filelist log1_dublin_notShifted/prediction_all.txt --room_data_filelist meta/area1_data_label_dublin.txt --area 1 --visu --jenis dublin  
 
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log2/epoch_49.ckpt --dump_dir log2/prediction --output_filelist log2/prediction_all.txt --room_data_filelist meta/area2_data_label_margonda.txt --area 2 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log2/epoch_49.ckpt --dump_dir log2/prediction --output_filelist log2/prediction_build_only.txt --room_data_filelist meta/area2_data_label_margonda.txt --area 2 --visu --jenis dublin  
+CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log3_dublin_notShifted/epoch_49.ckpt --dump_dir log3_dublin_notShifted/prediction --output_filelist log3_dublin_notShifted/prediction_all.txt --room_data_filelist meta/area3_data_label_dublin.txt --area 3 --visu --jenis dublin  
 
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log3/epoch_49.ckpt --dump_dir log3/prediction --output_filelist log3/prediction_all.txt --room_data_filelist meta/area3_data_label_margonda.txt --area 3 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log3/epoch_49.ckpt --dump_dir log3/prediction --output_filelist log3/prediction_build_only.txt --room_data_filelist meta/area3_data_label_margonda.txt --area 3 --visu --jenis dublin  
+CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log4_dublin_notShifted/epoch_49.ckpt --dump_dir log4_dublin_notShifted/prediction --output_filelist log4_dublin_notShifted/prediction_all.txt --room_data_filelist meta/area4_data_label_dublin.txt --area 4 --visu --jenis dublin  
 
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log4/epoch_49.ckpt --dump_dir log4/prediction --output_filelist log4/prediction_all.txt --room_data_filelist meta/area4_data_label_margonda.txt --area 4 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log4/epoch_49.ckpt --dump_dir log4/prediction --output_filelist log4/prediction_build_only.txt --room_data_filelist meta/area4_data_label_margonda.txt --area 4 --visu --jenis dublin  
+CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log5_dublin_notShifted/epoch_49.ckpt --dump_dir log5_dublin_notShifted/prediction --output_filelist log5_dublin_notShifted/prediction_all.txt --room_data_filelist meta/area5_data_label_dublin.txt --area 5 --visu --jenis dublin  
 
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log5/epoch_49.ckpt --dump_dir log5/prediction --output_filelist log5/prediction_all.txt --room_data_filelist meta/area5_data_label_margonda.txt --area 5 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log5/epoch_49.ckpt --dump_dir log5/prediction --output_filelist log5/prediction_build_only.txt --room_data_filelist meta/area5_data_label_margonda.txt --area 5 --visu --jenis dublin  
-
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log6/epoch_49.ckpt --dump_dir log6/prediction --output_filelist log6/prediction_all.txt --room_data_filelist meta/area6_data_label_margonda.txt --area 6 --visu --jenis dublin  
-CUDA_VISIBLE_DEVICES=2 python3 batch_inference_build_only.py --model_path log6/epoch_49.ckpt --dump_dir log6/prediction --output_filelist log6/prediction_build_only.txt --room_data_filelist meta/area6_data_label_margonda.txt --area 6 --visu --jenis dublin  
+CUDA_VISIBLE_DEVICES=2 python3 batch_inference.py --model_path log6_dublin_notShifted/epoch_49.ckpt --dump_dir log6_dublin_notShifted/prediction --output_filelist log6_dublin_notShifted/prediction_all.txt --room_data_filelist meta/area6_data_label_dublin.txt --area 6 --visu --jenis dublin  
